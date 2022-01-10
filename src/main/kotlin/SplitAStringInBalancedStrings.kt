134800994d861f5e28c0e835de13961a3b22cff7
SEOMOON CHOI <seomoonchoi@SEOMOONui-MacBookAir.local>
2022-01-10 23:44:40 +0900

    [LeetCode-1221] Split a String in Balanced Strings
    - 대칭이 되어야하는것처럼 보여서 헷갈렸던문제
    - count만 맞으면 split 한다

--- src/main/kotlin/SplitAStringInBalancedStrings.kt
+++ src/main/kotlin/SplitAStringInBalancedStrings.kt
@@ -1,2 +1,22 @@
+fun main() {
+    val solution = SplitAStringInBalancedStrings()
+    println(solution.balancedStringSplit("LLLLRRRR"))
+}
 class SplitAStringInBalancedStrings {
+    fun balancedStringSplit(s: String): Int {
+        var count = 0
+        var result = 0
+
+        s.map {
+            if (it == 'L') {
+                count++
+            } else {
+                count--
+            }
+            if (count == 0) {
+                result++
+            }
+        }
+        return result
+    }
 }
